e540ff04b3fd77f3cf8cfd35eeeac77996191cc4
TOURE O <ousmanealassane.toure@gmail.com>
2018-09-26 17:34:47 +0200

    Ajout emoji to <TaskList /> Component

--- js/app.js
+++ js/app.js
@@ -1,28 +1,45 @@
 class TaskList extends React.Component {
-  render() {
-    
-    const tasks = [
-    {
-      id: 1,
-      description: 'Go to bed',
-      complete: true
-    },
-    {
-      id: 2,
-      description: 'record a tutorial',
-      complete: false
-    },
-    {
-      id: 3,
-      description: 'Watch a movie',
-      complete: false
-    },
-    
-    ];
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      tasks: [
+        {
+          id: 1,
+          description: 'Go to bed',
+          complete: true
+        },
+        {
+          id: 2,
+          description: 'record a tutorial',
+          complete: false
+        },
+        {
+          id: 3,
+          description: 'Watch a movie',
+          complete: false
+        }
+    ]
+  
+  };
+}
+
+  render() {  
     return(
       <div>
-      <h1>All tasks</h1>
+        <h1>All tasks</h1>
+          {this.state.tasks.map((task, index) => {
+            // task va valoir chaque objet litteral
+            // map(task, index) => on récupère l"élément et son index : index 0, index 1, index 2
+            return (
+              <article key={index}>
+                <h1>
+                  #{task.id} - {task.description} 
+                  { task.complete ? ' ✅ ' : ' ❌' }
+                  </h1>
+              </article>
+              )
+          })}
       </div>
       )
   }
